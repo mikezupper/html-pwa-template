54d81572b7b27ef813e5ea79164fb81aaa12d6bc
Mike Zupper <mazup.x@gmail.com>
2021-05-20 17:48:08 -0400

    fixed final lighthouse recommendations

--- worker.js
+++
@@ -1,8 +0,0 @@
-if (window.Worker) {
-  const myWorker = new Worker("worker.js");
-  onmessage = (e) => {
-    console.log("Message received from main script");
-  };
-} else {
-  console.log("Your browser doesn't support web workers.");
-}
